a374e2516c7fcbbb7e50b4eb36a68adb31b6760d
ThatRandomCoderNZ <ethan.en.mitchell@gmail.com>
2021-12-07 19:57:49 +1300

    [feat] Now interprets messages from requests and sends it to Textfoo API

--- app/api/views.py
+++ app/api/views.py
@@ -2,13 +2,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import requests
+import json
+
 
 class SendMessagesView(APIView):
     def post(self, request):
         print("Accessed new API route")
-        query = {'key': '17eab8f4074305e411446df3a0d0acc9485f0fdc',
-                 'phone':'642108672121',
-                 'message':'message-to-myself'}
-        response = requests.get("https://textfoo.com/api/send", params=query)
-        print(response)
+
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        data = body['data']
+        for message in data:
+            query = {'key': '17eab8f4074305e411446df3a0d0acc9485f0fdc',
+                     'phone': message['number'].replace("0", "64", 1),
+                     'message': message['message']}
+            response = requests.get("https://textfoo.com/api/send", params=query)
+
         return Response({"status": "success", "data": ["placeholder"]}, status=status.HTTP_200_OK)
