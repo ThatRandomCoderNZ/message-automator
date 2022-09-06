df148f66ebb5af61862cdf261d20e0c86295215b
ThatRandomCoderNZ <ethan.en.mitchell@gmail.com>
2022-09-07 09:53:34 +1200

    initial commit

---
+++ static/app/InputForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <form>
+        <h1>Hello there!</h1>
+    </form>
+</template>
+
+
+<script>
+    module.exports = {
+        data: function() {
+            return{
+                something: "",
+            }
+        }
+    }
+</script>
